5dc8776841c0a6109a2c54d0d8f129fbe5038dad
jinwentao <1119145485@qq.com>
2021-03-08 21:23:17 +0800

    '完成了前端注册登录页面的布局样式'

--- info-mgr-fe/src/App.vue
+++ info-mgr-fe/src/App.vue
@@ -1,23 +1,6 @@
 <template>
-<!--  <div id="nav">-->
-<!--    <router-link to="/">Home</router-link> |-->
-<!--    <router-link to="/about">About</router-link>-->
-<!--  </div>-->
   <router-view/>
 </template>
-
-<script>
-export default {
-  data(){
-    return{
-
-    };
-  },
-  methods:{
-
-  },
-};
-</script>
 <style lang="scss">
 
 </style>
---
+++ info-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue';
+import { UserOutlined,LockOutlined,ContactsOutlined  } from '@ant-design/icons-vue';
+export default defineComponent({
+  components:{
+    UserOutlined,
+    LockOutlined,
+    ContactsOutlined,
+  },
+})
+
+;
